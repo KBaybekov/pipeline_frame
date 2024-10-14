19fbba2216e4872cb08b985f184cde2e967f7f92
KBaybekov <91565993+KBaybekov@users.noreply.github.com>
2024-10-14 17:40:36 +0300

    config_path > project_path

--- src/main_parser.py
+++ src/main_parser.py
@@ -6,11 +6,11 @@ def parse_args():
     # Парсим первый аргумент с путём к конфигу
     initial_args, remaining_args = parse_initial_args()
 
-    # Добавляем config_path в оставшиеся аргументы
-    remaining_args.extend(['--config_path', initial_args.config_path])
+    # Добавляем project_path в оставшиеся аргументы
+    remaining_args.extend(['--project_path', initial_args.project_path])
 
     # Загружаем и выполняем второй парсер, передаем остальные аргументы
-    parse_cli_args = load_config_parser(initial_args.config_path)
+    parse_cli_args = load_config_parser(initial_args.project_path)
     final_args = parse_cli_args()
 
     return final_args
@@ -20,17 +20,17 @@ def parse_initial_args():
     Парсер для получения пути к конфигу.
     """
     parser = argparse.ArgumentParser(description="Initial argument parser")
-    parser.add_argument('-cf', '--config_path', required=True, help="Путь для загрузки конфигурационных файлов")
+    parser.add_argument('-pp', '--project_path', required=True, help="Путь для загрузки конфигурационных файлов")
     
-    # Используем parse_known_args, чтобы собрать только --config_path и передать остальные аргументы позже
+    # Используем parse_known_args, чтобы собрать только --project_path и передать остальные аргументы позже
     args, remaining_args = parser.parse_known_args()
     return args, remaining_args
 
-def load_config_parser(config_path):
+def load_config_parser(project_path):
     """
     Импортирует второй парсер из указанного конфиг файла.
     """
-    arg_parser = os.path.join(config_path, 'arg_parser.py')
+    arg_parser = os.path.join(project_path, 'arg_parser.py')
     if not os.path.exists(arg_parser):
         raise FileNotFoundError(f"Парсер не найден: {arg_parser}")
 
