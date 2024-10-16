392f6a4222623501434b2ec1fa01c9850cc1ce91
KBaybekov <91565993+KBaybekov@users.noreply.github.com>
2024-10-16 23:57:05 +0300

    2357

--- src/module_runner.py
+++ src/module_runner.py
@@ -73,3 +73,9 @@ class ModuleRunner:
                 setattr(self, key, tuple(value))
                 continue
             setattr(self, key, value)
+
+            # Если ключ 'commands' и значение пустое, назначаем пустой словарь
+            if key == 'commands' and value is None:
+                setattr(self, key, {})
+            else:
+                setattr(self, key, value)
