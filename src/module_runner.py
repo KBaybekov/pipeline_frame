e71d3f672b12b6cd6fa4d4f36c1751c8cc8937dc
KBaybekov <91565993+KBaybekov@users.noreply.github.com>
2024-10-17 00:07:07 +0300

    007

--- src/module_runner.py
+++ src/module_runner.py
@@ -75,6 +75,7 @@ class ModuleRunner:
             # Если ключ 'commands' и значение пустое, назначаем пустой список
             if key == 'commands':
                 for group, value in data[key].items():
+                    print(group)
                     if value is None:
                         data[key][group] = []
             setattr(self, key, value)
